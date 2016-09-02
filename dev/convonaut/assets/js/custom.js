9647823565f1865771d552a8efb4fdf509e421ce
KurtLopez <kurtdevph@gmail.com>
2016-09-02 22:31:44 +0800

    convonaut website

---
+++ dev/convonaut/assets/js/custom.js
@@ -0,0 +1,226 @@
+/*=============================================================
+    Authour URL: www.designbootstrap.com
+    
+    http://www.designbootstrap.com/
+
+    License: MIT
+
+    http://opensource.org/licenses/MIT
+
+    100% Free To use For Personal And Commercial Use.
+
+    IN EXCHANGE JUST TELL PEOPLE ABOUT THIS WEBSITE
+   
+========================================================  */
+particlesJS('particles-js', 
+           
+           {
+              "particles": {
+                "number": {
+                  "value": 40,
+                  "density": {
+                    "enable": true,
+                    "value_area": 800
+                  }
+                },
+                "color": {
+                  "value": "#6ec7cd"
+                },
+                "shape": {
+                  "type": "circle",
+                  "stroke": {
+                    "width": 0,
+                    "color": "#000000"
+                  },
+                  "polygon": {
+                    "nb_sides": 5
+                  },
+                  "image": {
+                    "src": "img/github.svg",
+                    "width": 100,
+                    "height": 100
+                  }
+                },
+                "opacity": {
+                  "value": 0.3687847739990702,
+                  "random": false,
+                  "anim": {
+                    "enable": false,
+                    "speed": 1,
+                    "opacity_min": 0.1,
+                    "sync": false
+                  }
+                },
+                "size": {
+                  "value": 3,
+                  "random": true,
+                  "anim": {
+                    "enable": false,
+                    "speed": 40,
+                    "size_min": 0.1,
+                    "sync": false
+                  }
+                },
+                "line_linked": {
+                  "enable": true,
+                  "distance": 180,
+                  "color": "#ffffff",
+                  "opacity": 0.4,
+                  "width": 1
+                },
+                "move": {
+                  "enable": true,
+                  "speed": 2,
+                  "direction": "none",
+                  "random": true,
+                  "straight": false,
+                  "out_mode": "out",
+                  "bounce": false,
+                  "attract": {
+                    "enable": false,
+                    "rotateX": 600,
+                    "rotateY": 1200
+                  }
+                }
+              },
+              "interactivity": {
+                "detect_on": "canvas",
+                "events": {
+                  "onhover": {
+                    "enable": true,
+                    "mode": "bubble"
+                  },
+                  "onclick": {
+                    "enable": true,
+                    "mode": "repulse"
+                  },
+                  "resize": true
+                },
+                "modes": {
+                  "grab": {
+                    "distance": 400,
+                    "line_linked": {
+                      "opacity": 1
+                    }
+                  },
+                  "bubble": {
+                    "distance": 267.9854800594439,
+                    "size": 10,
+                    "duration": 0,
+                    "opacity": 0,
+                    "speed": 3
+                  },
+                  "repulse": {
+                    "distance": 200,
+                    "duration": 0.4
+                  },
+                  "push": {
+                    "particles_nb": 4
+                  },
+                  "remove": {
+                    "particles_nb": 2
+                  }
+                }
+              },
+              "retina_detect": true
+            }
+
+           );
+
+
+$(document).ready(function () {
+ 
+/*====================================
+SCROLLING SCRIPTS
+======================================*/
+
+$('.scroll-me a').bind('click', function (event) { //just pass scroll-me in design and start scrolling
+var $anchor = $(this);
+$('html, body').stop().animate({
+scrollTop: $($anchor.attr('href')).offset().top
+}, 1200, 'easeInOutExpo');
+event.preventDefault();
+});
+
+
+/*====================================
+SLIDER SCRIPTS
+======================================*/
+
+
+$('#carousel-slider').carousel({
+interval: 2000 //TIME IN MILLI SECONDS
+});
+
+
+/*====================================
+VAGAS SLIDESHOW SCRIPTS
+======================================*/
+$.vegas('slideshow', {
+backgrounds: [
+{ src: 'assets/img/1.jpg', fade: 1000, delay: 9000 },
+{ src: 'assets/img/2.jpg', fade: 1000, delay: 9000 },
+]
+})('overlay', {
+/** SLIDESHOW OVERLAY IMAGE **/
+src: 'assets/js/vegas/overlays/06.png' // THERE ARE TOTAL 01 TO 15 .png IMAGES AT THE PATH GIVEN, WHICH YOU CAN USE HERE
+});
+
+
+/*====================================
+POPUP IMAGE SCRIPTS
+======================================*/
+$('.fancybox-media').fancybox({
+openEffect: 'elastic',
+closeEffect: 'elastic',
+helpers: {
+title: {
+type: 'inside'
+}
+}
+});
+
+
+/*====================================
+FILTER FUNCTIONALITY SCRIPTS
+======================================*/
+$(window).load(function () {
+var $container = $('#work-div');
+$container.isotope({
+filter: '*',
+animationOptions: {
+duration: 750,
+easing: 'linear',
+queue: false
+}
+});
+$('.caegories a').click(function () {
+$('.caegories .active').removeClass('active');
+$(this).addClass('active');
+var selector = $(this).attr('data-filter');
+$container.isotope({
+filter: selector,
+animationOptions: {
+duration: 750,
+easing: 'linear',
+queue: false
+}
+});
+return false;
+});
+
+});
+
+
+
+/*====================================
+WRITE YOUR CUSTOM SCRIPTS BELOW
+======================================*/
+
+
+
+
+
+
+
+});
