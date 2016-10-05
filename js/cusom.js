a6a53e578edd7b36f19cf61e945ec33a1f078475
Ed Salinas <edsalinas7b@yahoo.com>
2016-10-05 17:16:51 +0800

    Make site live

---
+++ js/cusom.js
@@ -0,0 +1,18 @@
+(function(window){
+var bot = document.getElementById("phone");
+var top = -50;
+
+
+window.onscroll = function() {
+    var bodyHeight	= 800;
+    var scrollTop	= document.body.scrollTop; 
+    bot.style.top	= bodyHeight - scrollTop + "px" ;
+}     
+
+
+    
+})(window);
+
+particlesJS.load('particles-js', 'http://convonaut.com/dev/Knight/js/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
