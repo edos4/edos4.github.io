efd41ff2523d02b8ed616766994e92bd06678990
KurtLopez <kurtdevph@gmail.com>
2016-10-05 15:33:51 +0800

    changes header image, index.html, css and custom javascript

---
+++ thesis/HotelReservation/js/custom.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){       
+            var scroll_pos = 0;
+            $(document).scroll(function() { 
+                scroll_pos = $(this).scrollTop();
+                if(scroll_pos > 300) {
+                    $("#mainNav").css('background-color', 'rgba(201,183,138,.6)');
+                } else {
+                    $("#mainNav").css('background-color', 'transparent');
+                }
+            });
+
+
+            $(function() {
+              $('a[href*="#"]:not([href="#"])').click(function() {
+                if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                  var target = $(this.hash);
+                  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                  if (target.length) {
+                    $('html, body').animate({
+                      scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                  }
+                }
+              });
+            });
+});
